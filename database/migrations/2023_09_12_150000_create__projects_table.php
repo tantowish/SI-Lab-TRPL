a98e2616f2e4d1ebe9d8b0711fee2cb90669f90a
tanto <tantows001@gmail.com>
2023-09-13 14:44:49 +0700

    Database Migration update

--- database/migrations/2023_09_12_150000_create__projects_table.php
+++ database/migrations/2023_09_12_150000_create__projects_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->date('start_date');
             $table->date('end_date');
             $table->text('description');
+            $table->text('document_link');
         });
     }
 

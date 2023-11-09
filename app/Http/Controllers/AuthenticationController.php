7b61bfb20ede989007fec9ae268df8159a2d3f01
nauvaltr <naufaltrisubaktilbb123@gmail.com>
2023-11-09 09:52:50 +0700

    feat: landingpage index

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -8,46 +8,50 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view("page.auth.login");
     }
-    
-    public function auth(Request $request){
+
+    public function auth(Request $request)
+    {
         $request->validate([
-            "ugm_id"=> "required",
-            "password"=>"required"
+            "ugm_id" => "required",
+            "password" => "required"
         ]);
-        $request['email'] = $request['ugm_id']."@mail.ugm.ac.id";
+
+
+        $request['email'] = $request['ugm_id'] . "@mail.ugm.ac.id";
         // if (Auth::guard('web')->attempt(['email' => $request['email'], 'password' => 
         // $request['password']])) {
         //     $request->session()->regenerate();
         //     return redirect()->intended(route('dashboard'));
         // }
-        $credentials =[
-            "email"=>$request['email'],
-            "password"=>$request['password']
+        $credentials = [
+            "email" => $request['email'],
+            "password" => $request['password']
         ];
         if (Auth::attempt($credentials)) {
             session(['data' => Auth::user()]);
-            return redirect()->intended('dashboard');         
-        } 
+            return redirect()->intended('dashboard');
+        }
 
-        if (Auth::guard('admin')->attempt( $credentials )) {
+        if (Auth::guard('admin')->attempt($credentials)) {
             session(['data' => Auth::guard('admin')->user()]);
-            return redirect()->intended('dashboard');         
-        } 
+            return redirect()->intended('dashboard');
+        }
 
-         
+
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
-        ])->onlyInput(); 
+        ])->onlyInput();
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         session()->flush();
 
         return redirect('/login');
     }
-
 }
